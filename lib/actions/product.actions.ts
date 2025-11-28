459766fffb161b470a464cb064b44775652a5364
alkisax <alkisax@gmail.com>
2025-11-28 19:05:03 +0200

    deploy on vercel

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,11 +5,17 @@ import { prisma } from '@/db/prisma'
 
 // get latest products
 export async function getLatestProducts() {
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   })
 
   return convertToPlainObject(data)
+}
+
+// get single product by its slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  })
 }
